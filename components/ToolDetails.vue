68576ebc3a64c2ef5c9dac4e47cee317c6cdfc8f
Gwen <g@10degres.net>
2022-12-14 21:52:54 +0100

    mobile

--- components/ToolDetails.vue
+++ components/ToolDetails.vue
@@ -2,10 +2,10 @@
     <div id="tool-details" class="row">
         <div class="col-xl-8 col-lg-10 col-sm-12">
             <div class="row">
-                <div class="col col-8">
+                <div class="col-xl-8 col-lg-9 col-sm-11 col-11">
                     <img :src="'/assets/img/tools/'+tool.picture" class="rounded tool-picture img-fluid" />
                 </div>
-                <div class="col col-3 text-end">
+                <div class="col-xl-3 col-lg-2 col-sm-1 col-1 text-end">
                     <a href="javascript:;" v-on:click="doSearch()" class="link-close"><svg xmlns="http://www.w3.org/2000/svg" width="30" fill="currentColor" viewBox="0 0 512 512"><path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200zm101.8-262.2L295.6 256l62.2 62.2c4.7 4.7 4.7 12.3 0 17l-22.6 22.6c-4.7 4.7-12.3 4.7-17 0L256 295.6l-62.2 62.2c-4.7 4.7-12.3 4.7-17 0l-22.6-22.6c-4.7-4.7-4.7-12.3 0-17l62.2-62.2-62.2-62.2c-4.7-4.7-4.7-12.3 0-17l22.6-22.6c4.7-4.7 12.3-4.7 17 0l62.2 62.2 62.2-62.2c4.7-4.7 12.3-4.7 17 0l22.6 22.6c4.7 4.7 4.7 12.3 0 17z"/></svg></a>
                 </div>
             </div>
