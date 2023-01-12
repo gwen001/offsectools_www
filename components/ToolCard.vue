7bbecb61b9d0071baac539c9f58e37c7c6b39b30
Gwen <g@10degres.net>
2023-01-12 10:57:42 +0100

    seo

--- components/ToolCard.vue
+++ components/ToolCard.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="tool-card h-100">
         <div class="card h-100">
-            <img :src="'/assets/img/tools/'+tool.picture" class="card-img-top tool-picture" />
+            <nuxt-img :alt="tool.nicename+' on '+this.$config.APP_NAME" :src="'/assets/img/tools/'+tool.picture" placeholder="/assets/img/tools/default.png" class="card-img-top tool-picture" />
+            <!-- <img :alt="tool.nicename+' on '+this.$config.APP_NAME" :src="'/assets/img/tools/'+tool.picture" class="card-img-top tool-picture" /> -->
             <div class="card-body">
                 <div class="ratings-container" v-if="tool.ratings_count > 0">
                     <Ratings from="toolcard" :tool="tool"></Ratings>
