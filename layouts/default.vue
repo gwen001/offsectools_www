50bc2034299fbeb40513414508a4a98c1e9cf99d
Gwen <g@10degres.net>
2023-01-17 08:40:02 +0100

    mobile menu icon

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
             <div id="main" class="col p-0 m-0">
                 <Topbar></Topbar>
                 <div class="mobilemenu_icon d-lg-none d-xl-none">
-                        <a href="javascript:;" v-on:click="showMobileTarbar"><svg xmlns="http://www.w3.org/2000/svg" width="25" viewBox="0 0 448 512" fill="currentColor"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg></a>
+                        <a href="javascript:;" v-on:click="showMobileTarbar"><svg xmlns="http://www.w3.org/2000/svg" width="25" viewBox="0 0 448 512" fill="currentColor" stroke="#0a0c1f" stroke-width="20"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg></a>
                 </div>
                 <div class="row p-0 m-0">
                     <div class="col p-0 m-0">
@@ -64,5 +64,9 @@ a:hover {
     left: 8px;
     position: absolute;
     top: 8px;
+    z-index: 20;
+}
+.mobilemenu_icon svg {
+    /* border: 2px solid #f00; */
 }
 </style>
