99ad478adabf7e290a2182539829732046c9f77d
Gwen <g@10degres.net>
2023-01-29 15:00:01 +0100

    css

--- components/ToolContextualisation.vue
+++ components/ToolContextualisation.vue
@@ -1,18 +1,18 @@
 <template>
     <div id="contextualisation">
         <template v-if="t_context.length > 0">
-        <div class="row mb-2">
-            <div class="col">
-                <h6>You might be interested by:</h6>
-            </div>
-        </div>
-        <div class="row">
-            <template v-for="tool,index in t_context">
-                <div class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-6 mb-3">
-                    <ToolCard :tool="tool" from="contextualisation"></ToolCard>
+            <div class="row mb-2">
+                <div class="col">
+                    <h6>You might be interested by:</h6>
                 </div>
-            </template>
-        </div>
+            </div>
+            <div class="row">
+                <template v-for="tool,index in t_context">
+                    <div class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-6 mb-3">
+                        <ToolCard :tool="tool" from="contextualisation"></ToolCard>
+                    </div>
+                </template>
+            </div>
         </template>
     </div>
 </template>
@@ -38,17 +38,5 @@ export default {
 }
 </script>
 
-<style scope>
-#contextualisation div.row:first-of-type {
-    margin-top: 100px;
-}
-#contextualisation .card {
-    font-size: 0.75em;
-}
-/* #contextualisation .tool-name {
-    font-size: 1em;
-} */
-#contextualisation img {
-    height: 100px;
-}
+<style scoped>
 </style>
